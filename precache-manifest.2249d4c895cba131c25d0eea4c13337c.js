b821236e42d74ce78afb21596154177accc8bf45
Yassin <yag@silico.fr>
2020-01-21 18:09:16 +0100

    Updates

---
+++ precache-manifest.2249d4c895cba131c25d0eea4c13337c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d5e3630b594afe1614c2379faef67c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b876d4ac504af54f8ad",
+    "url": "/monsters-rolodex/static/css/main.04156bb2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b876d4ac504af54f8ad",
+    "url": "/monsters-rolodex/static/js/main.e95990b9.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
